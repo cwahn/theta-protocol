33b55f4591ad4c71cf9e83c4c69cad9a8a23c527
Chanwoo Ahn <cwahn@arsvivendi.io>
2025-08-05 11:56:50 +0900

    refactor: Refactor trait into two

--- src/connection.rs
+++ src/connection.rs
@@ -1,18 +1,32 @@
 #[cfg(feature = "alloc")]
 mod connection_impl {
+    use alloc::boxed::Box;
     #[cfg(all(feature = "alloc", not(feature = "std")))]
     use alloc::vec::Vec;
     #[cfg(feature = "std")]
     use std::vec::Vec;
 
-    use futures::future::BoxFuture;
     use crate::error::Error;
+    use futures::future::BoxFuture;
 
-    pub trait Connection: Send + Sync {
+    // Should be one per host
+    pub trait Connection: Send + Sync + Sender + Receiver {
+        fn open_uni(&self) -> BoxFuture<Result<Box<dyn Sender>, Error>>;
+
+        fn accept_uni(&self) -> BoxFuture<Result<Box<dyn Receiver>, Error>>;
+    }
+
+    // Logical sender
+    // It could be actual stream in case of WebSocket like transport, or internally wrap message with stream_id and send to single internal stream.
+    pub trait Sender: Send + Sync {
         /// Send exactly one datagram to the connected peer.
         /// The transport guarantees integrity‐checked, at‐most‐once delivery.
         fn send_datagram(&self, payload: Vec<u8>) -> BoxFuture<Result<(), Error>>;
+    }
 
+    // Logical receiver
+    // It could be actual stream in case of WebSocket like transport, or internally wrap message from single internal stream.
+    pub trait Receiver: Send + Sync {
         /// Receive the next datagram from the peer.
         fn recv_datagram(&self) -> BoxFuture<Result<Vec<u8>, Error>>;
     }
